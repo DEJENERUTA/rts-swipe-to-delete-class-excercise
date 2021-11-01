95126a82934ccd03f86a0df41c2ec73492c461f4
Dejene <dejenedaba269@gmail.com>
2021-11-01 14:15:40 +0100

    swipe to left is work

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const jokeItem = document.querySelector(".jokeItem");
 let touchCordinateStart;
 let touchCordinateMove;
+let deleteButtonWidth = (window.screen.width * 40) / 100;
 
 jokeItem.addEventListener("touchstart", (e) => {
   touchCordinateStart = e.touches[0].clientX;
 });
 jokeItem.addEventListener("touchmove", (e) => {
   touchCordinateMove = Math.floor(e.touches[0].clientX);
-  if (touchCordinateMove < touchCordinateStart) {
+  if (
+    touchCordinateMove < touchCordinateStart &&
+    touchCordinateMove > touchCordinateStart - deleteButtonWidth
+  ) {
     jokeItem.style.transform = `translateX(${
       touchCordinateMove - touchCordinateStart
     }px)`;
