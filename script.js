d5b3c2aeace59927f965919b3b689a1bfd1ef445
Dejene <dejenedaba269@gmail.com>
2021-11-01 14:53:12 +0100

    back and forth swipe is working

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const jokeItem = document.querySelector(".jokeItem");
 let touchCordinateStart;
 let touchCordinateMove;
+let touchCordinateEnd;
 let deleteButtonWidth = (window.screen.width * 40) / 100;
 
 jokeItem.addEventListener("touchstart", (e) => {
@@ -15,5 +16,13 @@ jokeItem.addEventListener("touchmove", (e) => {
     jokeItem.style.transform = `translateX(${
       touchCordinateMove - touchCordinateStart
     }px)`;
+  }
+});
+jokeItem.addEventListener("touchend", (e) => {
+  touchCordinateEnd = Math.floor(e.changedTouches[0].clientX);
+  if (touchCordinateEnd < touchCordinateStart - deleteButtonWidth / 2) {
+    jokeItem.style.transform = `translateX(-${deleteButtonWidth}px)`;
+  } else {
+    jokeItem.style.transform = `translateX(0)`;
   }
 });
