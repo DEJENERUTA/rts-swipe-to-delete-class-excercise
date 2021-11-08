36e41237e9c14a64a8749dc79b5a783d525ea6da
Dejene <dejenedaba269@gmail.com>
2021-11-08 08:47:22 +0100

    minor change

--- trash.js
+++ trash.js
@@ -1,1 +1,6 @@
-let userObject = localStorage.getItem("userObject");
+let deleteItem = JSON.parse(localStorage.getItem("deleteItem"));
+/* console.log(deleteItem); */
+deleteItem.forEach((element) => {
+  let result = JSON.parse(element);
+  console.log(result.id);
+});
